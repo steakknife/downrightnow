9062cd7d4add97c4d6963406cbf1ad8d689bab1f
Barry Allard <barry@barryallard.name>
2011-10-14 05:26:48 -0700

    fixed a library search path issue

--- lib/downrightnow/util/command_line.rb
+++ lib/downrightnow/util/command_line.rb
@@ -7,20 +7,21 @@ module DownRightNow
       def exec
 	begin
 	  opts = Trollop::options do
-	    version "DownRightNow (tm) unofficial ruby utility #{DownRightNow.version}  (c) 2011 Barry Allard"
+	    version "DownRightNow (tm) unofficial ruby utility #{version}  (c) 2011 Barry Allard"
 	    banner <<-EOS
-	DownRightNow (tm) unofficial ruby utility #{DownRightNow.version}  (c) 2011 Barry Allard
+	DownRightNow (tm) unofficial ruby utility #{version}  (c) 2011 Barry Allard
 
 	    EOS
 	    opt :verbose,         "Verbose display", :default => false
 	    opt :error_on_outage, "Return error exit code if any outage", :default => false
 	  end
-	  drn = DownRightNow::DownRightNow.new
+	  drn = DownRightNow.new
 	  puts drn.to_s opts[:verbose]
 	  exit 1 if opts[:error_on_outage] && drn.any_outages?
 	rescue => e
-	  puts e
-	  exit 1
+#	  puts e
+#	  exit 1
+          raise e
 	end
       end # def exec
     end # class CommandLine
